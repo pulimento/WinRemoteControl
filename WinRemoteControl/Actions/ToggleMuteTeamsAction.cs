db3bf81f66b62f8adf929a09720f6777c4d979b8
Javi Pulido <pulimento@gmail.com>
2022-03-08 12:54:57 +0100

    Create actions

---
+++ WinRemoteControl/Actions/ToggleMuteTeamsAction.cs
@@ -0,0 +1,34 @@
+﻿using FluentResults;
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Serilog;
+using System.Diagnostics;
+using System.Linq;
+
+namespace WinRemoteControl.Actions
+{
+    class ToggleMuteTeamsAction : IAction
+    {
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+        public Result DoAction()
+        {
+            Log.Information("Toggling Teams mute");
+
+            Process? p = Process.GetProcessesByName("Teams").FirstOrDefault();
+            if (p != null)
+            {
+                IntPtr h = p.MainWindowHandle;
+                SetForegroundWindow(h);
+                SendKeys.SendWait("^+{m}"); // CTRL + SHIFT + M
+            }
+            else
+            {
+                Log.Error("Can't find Teams process. Is it running?");
+            }
+            return Result.Ok();
+        }
+    }
+}

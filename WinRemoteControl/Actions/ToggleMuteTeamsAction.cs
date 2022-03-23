324f922624f61c9eb10142d522b8e6b71b5ef6b3
Francisco Javier Pulido Espina <jpulido@plainconcepts.com>
2022-03-23 18:10:59 +0100

    Inline namespaces, global usings from C# 10

--- WinRemoteControl/Actions/ToggleMuteTeamsAction.cs
+++ WinRemoteControl/Actions/ToggleMuteTeamsAction.cs
@@ -1,34 +1,29 @@
 ﻿using FluentResults;
-using System;
 using System.Runtime.InteropServices;
-using System.Windows.Forms;
-using Serilog;
 using System.Diagnostics;
-using System.Linq;
 
-namespace WinRemoteControl.Actions
+namespace WinRemoteControl.Actions;
+
+class ToggleMuteTeamsAction : IAction
 {
-    class ToggleMuteTeamsAction : IAction
+    [DllImport("User32.dll")]
+    static extern int SetForegroundWindow(IntPtr point);
+
+    public Result DoAction()
     {
-        [DllImport("User32.dll")]
-        static extern int SetForegroundWindow(IntPtr point);
+        Log.Information("Toggling Teams mute");
 
-        public Result DoAction()
+        Process? p = Process.GetProcessesByName("Teams").FirstOrDefault();
+        if (p != null)
         {
-            Log.Information("Toggling Teams mute");
-
-            Process? p = Process.GetProcessesByName("Teams").FirstOrDefault();
-            if (p != null)
-            {
-                IntPtr h = p.MainWindowHandle;
-                SetForegroundWindow(h);
-                SendKeys.SendWait("^+{m}"); // CTRL + SHIFT + M
-            }
-            else
-            {
-                Log.Error("Can't find Teams process. Is it running?");
-            }
-            return Result.Ok();
+            IntPtr h = p.MainWindowHandle;
+            SetForegroundWindow(h);
+            SendKeys.SendWait("^+{m}"); // CTRL + SHIFT + M
         }
+        else
+        {
+            Log.Error("Can't find Teams process. Is it running?");
+        }
+        return Result.Ok();
     }
 }
---
+++ WinRemoteControl/Updater/Updater.cs
@@ -0,0 +1,15 @@
+﻿using AutoUpdaterDotNET;
+
+namespace WinRemoteControl.Updater;
+
+internal class Updater
+{
+    public static void CheckForUpdates()
+    {
+        string updaterURL = Environment.Is64BitProcess ?
+            Constants.UPDATE_URL_WIN64 :
+            Constants.UPDATE_URL_WIN32;
+
+        AutoUpdater.Start(updaterURL);
+    }
+}
